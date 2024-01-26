da50761d5bdd5b41d6f517b94183b78d38219404
Jessica Gates <gatespj@outlook.com>
2024-01-25 20:59:43 -0500

    final changes

--- NumberAnalyzerLab/NumberAnalyzerLab/Program.cs
+++ NumberAnalyzerLab/NumberAnalyzerLab/Program.cs
@@ -1,34 +1,49 @@
 ﻿using System.ComponentModel.Design;
-
+    
 Console.WriteLine("Hi! What is your name?");
 string name = Console.ReadLine();
 
-Console.WriteLine(name + " ,please enter a number between 1 and 100:");
-int userNumber = int.Parse(Console.ReadLine());
+bool keepGoing;
 
-bool even = userNumber % 2 == 0;
+do
+{
+    Console.WriteLine(name + ", please enter a number between 1 and 100:");
+    int userNumber = int.Parse(Console.ReadLine());
 
-if (!even && userNumber < 60)
+    bool even = userNumber % 2 == 0;
+
+    if (!even && userNumber < 60 && userNumber >= 1)
     {
-        Console.WriteLine(userNumber + " is odd and less than 60, " + name ".");
+        Console.WriteLine(userNumber + " is odd and less than 60, " + name + ".");
     }
-    else if (even && userNumber >= 2 && userNumber <= 24)
+    if (even && userNumber >= 2 && userNumber <= 24)
     {
-        Console.WriteLine(name + " ,your number is even and less than 25.");
+        Console.WriteLine(name + ", your number is even and less than 25.");
     }
-    else if (even && userNumber >= 26 && userNumber <= 60)
+    if (even && userNumber >= 26 && userNumber <= 60)
     {
         Console.WriteLine(userNumber + " is even and between 26 and 60 inclusive, " + name + ".");
     }
-    else if (!even && userNumber > 60)
+    if (even && userNumber > 60 && userNumber <= 100)
+    {
+        Console.WriteLine(userNumber + " is even and greater than 60, " + name + ".");
+    }
+    if (!even && userNumber > 60 && userNumber <=100)
     {
         Console.WriteLine(userNumber + " is odd and greater than 60, " + name + ".");
     }
-    else
+    else if (userNumber > 100 || userNumber < 1)
     {
-        Console.WriteLine(///don't know what to put here yet come back to tomorrow);
-                }
+        Console.WriteLine("Incorrect input. Please enter a number between 1 and 100");
+    }
+    
+    Console.WriteLine(name + ", do you want to continue? (yes/no)");
+    keepGoing = Console.ReadLine().ToLower().Trim() == "yes";
+    
+}
+    while (keepGoing) ;
+    {
+        Console.WriteLine("Bye " + name + "!");
+    }
 
-
-
-
+Console.ReadKey();
